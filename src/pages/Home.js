83c468d40e7e98ec95a6a60239109a240c4255fd
naveen8801 <naveensharma10d@gmail.com>
2022-03-01 22:19:56 +0530

    deployed to github pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { makeStyles, Typography, Card } from '@material-ui/core';
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  contactHeading: {
+    fontFamily: 'Montserrat',
+    fontWeight: 600,
+    color: 'white',
+    marginBottom: '2rem',
+  },
+  btn: {
+    backgroundColor: '#78B64D!important',
+    borderColor: '#78B64D!important',
+  },
+}));
 
 function Home() {
-  return <div>Home</div>;
+  const classes = useStyles();
+
+  return <div className={classes.root}>Home</div>;
 }
 
 export default Home;
