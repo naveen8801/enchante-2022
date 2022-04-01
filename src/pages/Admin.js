50746f7636dbf548e1ced55214b19f2406d91a93
naveen8801 <naveensharma10d@gmail.com>
2022-04-01 10:45:08 +0530

    ui fix and deploy

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -21,7 +21,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const useStyles = makeStyles(() => ({
   root: {
-    minHeight: '94vh',
+    height: '80vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -44,8 +44,10 @@ const useStyles = makeStyles(() => ({
     borderColor: '#78B64D!important',
   },
   card: {
+    height: '70vh',
     width: '80%',
     textAlign: 'center',
+    overflow: 'auto',
   },
 }));
 
